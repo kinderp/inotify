ac74a7ca5b17e909023eb17b165f6889cf7ed4e4
kinderp <a.caristia@gmail.com>
2025-05-02 16:36:43 +0000

    Move from main to utilwq
    void init_ptr_char_v(int dim, int is_realloc)
    int add_elem_in_ptr_char_v(char **head, char *elem, int dim)

--- util.c
+++ util.c
@@ -2,6 +2,10 @@
 #define __UTIL_H__
 
 #include<time.h>
+#include<string.h>
+#include<stdlib.h>
+
+extern char **TRIGGERED_NAMES;
 
 char *FILE_TYPE(int t){
         return t == 1 ? "DIR" : "FILE";
@@ -9,6 +13,33 @@ char *FILE_TYPE(int t){
 
 time_t get_timestamp(void){
 	return time(NULL);
+}
+
+void init_ptr_char_v(int dim, int is_realloc){
+        if(is_realloc){
+                TRIGGERED_NAMES = (char **)realloc(TRIGGERED_NAMES, dim*sizeof(char *));
+                for(int j=is_realloc; j<dim; j++)
+                        TRIGGERED_NAMES[j] = NULL;
+                return;
+        }
+
+        TRIGGERED_NAMES = (char **)malloc(dim*sizeof(char *)); // path of the filenamesthe triggred an event
+        for(int i=0; i<dim; i++)
+                TRIGGERED_NAMES[i] = NULL;
+}
+
+int add_elem_in_ptr_char_v(char **head, char *elem, int dim){
+        int i;
+        for(i=0; head[i]!=NULL && i<dim; i++){
+                if(strcmp(head[i], elem)==0)
+                        return i;
+        }
+        // if here we didn't find elem
+        if(i<dim && head[i] == NULL){
+                head[i] = elem;
+                return i;
+        }
+        return -1; // no more space
 }
 
 #endif
--- util.h
+++ util.h
@@ -5,5 +5,6 @@
 
 char *FILE_TYPE(int t);
 time_t get_timestamp(void);
-
+void init_ptr_char_v(int dim, int is_realloc);
+int add_elem_in_ptr_char_v(char **head, char *elem, int dim);
 #endif
